ec3173812a9e1b3302b408a2197d713a45edfb64
EnzoGRDev <enzo.g.rz.18@gmail.com>
2021-11-24 22:25:25 -0300

    fix: v.0.1.1 refactorización de pagina Doctors, reordenamiento de archivos, creado componenteFooter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/nav/Navbar';
+import Navbar from './components/Navbar';
 import Clinic from './pages/Clinic';
 import Doctors from './pages/Doctors';
 import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/Doctors" exact component={Doctors} />
         <Route path="/Clinic" exact component={Clinic} />
       </Switch>
+      <Footer/>
       </Router>
     </>
   );
