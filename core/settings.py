6e575cc1bcb5b101ea3d63809e2ee834c2023d90
JJWizardMP <jj.mpwizard@gmail.com>
2021-12-12 04:25:45 -0500

    final

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
+heroku config:set DISABLE_COLLECTSTATIC=1
 
 from pathlib import Path
 import environ
