ac24c0a114fd17bd25d98049ed0ae2b2ca8e3e4e
JJWizardMP <jj.mpwizard@gmail.com>
2021-12-12 04:16:24 -0500

    second try

--- backend/medicalbackend/core/settings.py
+++ backend/medicalbackend/core/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
+
 from pathlib import Path
 import environ
-import django_heroku
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
