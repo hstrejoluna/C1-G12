2a32b418678705b9c08a502d87efa4d4c2618a46
JJWizardMP <jj.mpwizard@gmail.com>
2021-12-12 21:39:39 -0500

    cords origins configured

--- core/settings.py
+++ core/settings.py
@@ -55,12 +55,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.common.CommonMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
@@ -182,8 +182,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "htpps://clinic-beta-vercel.app",
-    "htpp://localhost:3000",
+    "https://clinic-beta-vercel.app",
+    "http://localhost:3000",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     "http://192.168.2.205:3000"
